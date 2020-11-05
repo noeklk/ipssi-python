1fdbc258ae22ec20f712619e405abf5776412e87
Noe ABDEL KALEK <noe.abdelkalek@rentacar.fr>
2020-11-05 12:53:31 +0100

    ajout notebook pandas et ajout module

--- interface/app.py
+++ interface/app.py
@@ -13,9 +13,6 @@ SECRET_KEY = secrets.token_urlsafe(16)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-# if __name__ == "__main__":
-#     app.run()
-
 def set_bankroll(bankroll: int):
     session['bankroll'] = bankroll
 def get_bankroll():
